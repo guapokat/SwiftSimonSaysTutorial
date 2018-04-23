76518d9a2ce0028106a39ce0430b770bc51ef750
guapokat <virgilmartinez@gmail.com>
2018-04-23 18:42:07 -0500

    Project is now done. Has multiplayer functionality and some crisp UI (with slight animations). Overall a very good learning experience and a neat little toy.

---
+++ Simon Says/CircularButton.swift
@@ -0,0 +1,28 @@
+//
+//  CircularButton.swift
+//  Simon Says
+//
+//  Created by Virgil Martinez on 4/22/18.
+//  Copyright © 2018 Virgil Alexander Martinez. All rights reserved.
+//
+
+import UIKit
+
+class CircularButton: UIButton {
+
+    override func awakeFromNib() {
+        layer.cornerRadius = frame.size.width/2
+        layer.masksToBounds = true
+    }
+    
+    override var isHighlighted: Bool {
+        didSet {
+            if isHighlighted {
+                alpha = 1.0
+            } else {
+                alpha = 0.5
+            }
+        }
+    }
+
+}
